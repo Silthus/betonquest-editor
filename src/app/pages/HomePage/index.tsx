ecfac22bf51bf6b19ba2f58bb22c59b90e0a88db
Silthus <michael@reichenbach.in>
2022-06-25 06:52:48 +0200

    reproduce tailwindcss bug
    react-boilerplate/react-boilerplate-cra-template#175

--- src/app/pages/HomePage/index.tsx
+++ src/app/pages/HomePage/index.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import { Helmet } from 'react-helmet-async';
-import { Flow } from '../../components/Flow/Flow';
 
 export function HomePage() {
   return (
@@ -9,9 +8,7 @@ export function HomePage() {
         <title>HomePage</title>
         <meta name="description" content="A Boilerplate application homepage" />
       </Helmet>
-      <div style={{ height: '100vh' }}>
-        <Flow></Flow>
-      </div>
+      <button className="btn btn-primary">Hello Tailwind</button>
     </>
   );
 }
